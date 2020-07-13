0d09060593966656716f4aa385f9b3ef2beea240
MattT <trojanface@hotmail.com>
2020-07-13 17:59:36 +0930

    Updates

---
+++ precache-manifest.74415c60722e972a0969c0c96dd1f4ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6948a137e6b6fa36fcb5b26adcd9823c",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "77eaa648923e2b14cace",
+    "url": "/reactportfolio/static/css/main.6cef0535.chunk.css"
+  },
+  {
+    "revision": "100a6a85cd9cabae3a0a",
+    "url": "/reactportfolio/static/js/2.0fa76def.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.0fa76def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77eaa648923e2b14cace",
+    "url": "/reactportfolio/static/js/main.cd049af1.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  },
+  {
+    "revision": "0e74a230e58e85c867c13fe49040c273",
+    "url": "/reactportfolio/static/media/Homedes.0e74a230.png"
+  },
+  {
+    "revision": "33c4469ae9ff0f80fac03736cf57659e",
+    "url": "/reactportfolio/static/media/desktop.33c4469a.png"
+  },
+  {
+    "revision": "0d54b7fedb6fce2614641cee670db430",
+    "url": "/reactportfolio/static/media/kicksU.0d54b7fe.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactportfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1093a5059078546cabc96acfdc33bcde",
+    "url": "/reactportfolio/static/media/screenshot.1093a505.png"
+  }
+]);
